79ef4d50b6e81d15f21ce9ce5bf0c64951564406
TabGuima <tab.guimaraes@gmail.com>
2022-05-15 21:23:41 -0300

    inicio do script de calculo orçamento

--- PROJETO WEBSITE/assets/js/main.js
+++ PROJETO WEBSITE/assets/js/main.js
@@ -4,3 +4,10 @@ document
 	.querySelector('.hamburguer')
 	.addEventListener('click', () => document.querySelector('.container').classList.toggle('show-menu'));
 //
+
+document.querySelector('#qtde').addEventListener('change', atualizarPreco);
+
+function atualizarPreco() {
+	const qtde = document.querySelector('#qtde').value;
+	console.log(qtde);
+}
