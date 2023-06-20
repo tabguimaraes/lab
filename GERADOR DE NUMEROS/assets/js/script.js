7204b9ef1680e5b5980e2fb7464521e1da70f3da
TabGuima <tab.guimaraes@gmail.com>
2023-06-20 08:18:25 -0300

    add funcao executar numeros. testes ok

--- GERADOR DE NUMEROS/assets/js/script.js
+++ GERADOR DE NUMEROS/assets/js/script.js
@@ -1,12 +1,38 @@
-console.log('OK');
+console.log("OK");
 
-document.querySelector('#gerar').addEventListener('click', somar);
-let resultado = 0;
+document.querySelector("#gerar").addEventListener("click", executar);
 
-function somar(min, max) {
-	// min = Math.ceil(1);
-	// max = Math.floor(60);
-	min = 1;
-	max = 60;
-	console.log(Math.floor(Math.random() * (max - min + 1)) + min); //adicionar esse resultado dentro de um array e limitar ele a 6 números diferentes e em ordem crescente
+exibir = document.querySelector("#exibir");
+
+function executar() {
+  // Função para gerar números aleatórios
+  function gerarNumero(min, max) {
+    const r = Math.random() * (max - min) + min;
+    // Método para arredondar o resultado para baixo, transformando o número em inteiro
+    return Math.floor(r);
+  }
+
+  // Chamar a função e gerar de numeros de 1 a 60
+  let numeroAleatorio = gerarNumero(1, 60);
+
+  //Array para salvar os números
+  let listaDeNumeros = [];
+
+  // laço para salvar 6 números únicos dentro do array 'ListaDeNumeros'
+  while (listaDeNumeros.length < 6) {
+    listaDeNumeros.includes(numeroAleatorio)
+      ? (numeroAleatorio = gerarNumero(1, 60))
+      : listaDeNumeros.push(numeroAleatorio);
+  }
+
+  // Organizar o array por ordem crescente
+  resultado = listaDeNumeros.sort(function (a, b) {
+    return a - b;
+  });
+
+  console.log("##########################");
+  console.log(resultado);
+  console.log("##########################");
+  exibir.innerHTML = resultado;
+  // console.log(gerarNumero);
 }
