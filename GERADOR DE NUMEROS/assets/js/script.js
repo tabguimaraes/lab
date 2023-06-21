10eb07740728eb9fb5828ca85046cef76cefe3af
TabGuima <tab.guimaraes@gmail.com>
2023-06-21 10:04:32 -0300

    ajuste de funcoes, restricoes de range, etc

--- GERADOR DE NUMEROS/assets/js/script.js
+++ GERADOR DE NUMEROS/assets/js/script.js
@@ -1,38 +1,62 @@
 console.log("OK");
 
 document.querySelector("#gerar").addEventListener("click", executar);
+document.querySelector("#jogos").addEventListener("change", atualizar);
+const jogos = document.querySelector("#jogos");
+const exibir = document.querySelector("#exibir");
+let qtde;
 
-exibir = document.querySelector("#exibir");
+function atualizar() {
+  qtde = jogos.value;
+  // Verificar se qtde de jogos é maior que zero e menor que 3 jogos:
+  if (qtde <= 0) {
+    exibir.innerHTML = "Quantidade não pode ser menor ou igual a zero";
+  }
+  if (qtde > 3) {
+    exibir.innerHTML = "Quantidade não pode ser maior que 3 jogos";
+  }
+}
 
 function executar() {
-  // Função para gerar números aleatórios
-  function gerarNumero(min, max) {
-    const r = Math.random() * (max - min) + min;
-    // Método para arredondar o resultado para baixo, transformando o número em inteiro
-    return Math.floor(r);
+  atualizar();
+
+  if (qtde > 0 && qtde <= 3) {
+    exibir.innerText = "";
+    i = 0;
+    while (i < qtde) {
+      // Função para gerar números aleatórios
+      function gerarNumero(min, max) {
+        const r = Math.random() * (max - min) + min;
+        // Método para arredondar o resultado para baixo, transformando o número em inteiro
+        return Math.floor(r);
+      }
+
+      // Chamar a função e gerar de numeros de 01 a 60
+      let numeroAleatorio = gerarNumero(1, 61);
+
+      //Array para salvar os números
+      let listaDeNumeros = [];
+
+      // laço para salvar 6 números únicos dentro do array 'ListaDeNumeros'
+      while (listaDeNumeros.length < 6) {
+        listaDeNumeros.includes(numeroAleatorio)
+          ? (numeroAleatorio = gerarNumero(1, 61))
+          : listaDeNumeros.push(numeroAleatorio);
+      }
+
+      // Organizar o array por ordem crescente
+      const resultado = listaDeNumeros.sort(function (a, b) {
+        return a - b;
+      });
+
+      // console.log("##########################");
+      // console.log(resultado);
+      // console.log("##########################");
+      const inserirResultado = document.createElement("p");
+      inserirResultado.innerText = resultado;
+      exibir.appendChild(inserirResultado);
+      i++;
+    }
+    console.log(i);
   }
-
-  // Chamar a função e gerar de numeros de 1 a 60
-  let numeroAleatorio = gerarNumero(1, 60);
-
-  //Array para salvar os números
-  let listaDeNumeros = [];
-
-  // laço para salvar 6 números únicos dentro do array 'ListaDeNumeros'
-  while (listaDeNumeros.length < 6) {
-    listaDeNumeros.includes(numeroAleatorio)
-      ? (numeroAleatorio = gerarNumero(1, 60))
-      : listaDeNumeros.push(numeroAleatorio);
-  }
-
-  // Organizar o array por ordem crescente
-  resultado = listaDeNumeros.sort(function (a, b) {
-    return a - b;
-  });
-
-  console.log("##########################");
-  console.log(resultado);
-  console.log("##########################");
-  exibir.innerHTML = resultado;
-  // console.log(gerarNumero);
 }
