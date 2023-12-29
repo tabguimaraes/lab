78deff49b84d0d383b9449f826da592aba668614
TabGuima <tab.guimaraes@gmail.com>
2023-12-28 23:36:37 -0300

    codigo funcionando

--- SANDBOX/BUSCA_CEP/assets/js/main.js
+++ SANDBOX/BUSCA_CEP/assets/js/main.js
@@ -2,13 +2,22 @@ console.log("ok");
 
 const cep = document.querySelector("#cep");
 const btn = document.querySelector("#btn");
+const resultado = document.querySelector("#resultado");
 
 function buscarCEP() {
   btn.addEventListener("click", () => {
-    // if( )
-
     const cepDigitado = `https://viacep.com.br/ws/${cep.value}/json`;
-    console.log(cepDigitado);
+    fetch(cepDigitado).then(function (response) {
+      response
+        .json()
+        .then((response) => {
+          return response;
+        })
+        .then((response) => {
+          console.log(response);
+          resultado.innerText += `${response.logradouro} - ${response.bairro} - ${response.localidade}/${response.uf}`;
+        });
+    });
   });
 }
 
