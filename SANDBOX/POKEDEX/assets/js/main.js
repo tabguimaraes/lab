56213cb9c0d03e1fb42665009a58c008decc0242
TabGuima <tab.guimaraes@gmail.com>
2024-01-01 20:28:56 -0300

    codigo js ok

--- SANDBOX/POKEDEX/assets/js/main.js
+++ SANDBOX/POKEDEX/assets/js/main.js
@@ -1,5 +1,3 @@
-console.log("Conexão ok");
-
 const container = document.querySelector(".container");
 /* let novoElemento;
 
@@ -17,32 +15,66 @@ elementos.forEach((item) => {
 });
  */
 
-/* INSERIR FUNÇÃO NO FORM DE BUSCA VIA ADDEVENTLISTENER */
-function searchPokemon(pokemon) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(async (pokemon) => {
-    await pokemon.json().then((pokemon) => {
-      insertSprite(pokemon);
-      insertData(pokemon);
+function initPokemon() {
+  /* INSERIR FUNÇÃO NO FORM DE BUSCA VIA ADDEVENTLISTENER */
+  function searchPokemon(pokemon) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then(async (pokemon) => {
+      await pokemon.json().then((pokemon) => {
+        insertSprite(pokemon);
+        insertData(pokemon);
+        return;
+      });
     });
-  });
+  }
+  searchPokemon("1");
+
+  function insertSprite(pokemon) {
+    const image = pokemon["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+    const pokemonSprite = document.querySelector(".pokemonSprite");
+    return pokemonSprite.setAttribute("src", image);
+  }
+
+  function insertData(pokemon) {
+    const pokemonName = document.querySelector(".pokemonName");
+    const pokemonType = document.querySelector(".pokemonType");
+    const pokemonID = document.querySelector(".pokemonID");
+
+    pokemonName.innerText = pokemon["name"];
+    pokemonType.innerText = pokemon["types"]["0"]["type"]["name"];
+    pokemonID.innerText = pokemon["id"];
+    return;
+  }
+
+  function btnSearch() {
+    const btnNext = document.querySelector(".next");
+    const btnPrevious = document.querySelector(".previous");
+
+    btnNext.addEventListener("click", () => {
+      const pokemonID = +document.querySelector(".pokemonID").innerText + 1;
+      searchPokemon(pokemonID);
+    });
+    btnPrevious.addEventListener("click", () => {
+      const pokemonID = +document.querySelector(".pokemonID").innerText - 1;
+      if (pokemonID < 1) {
+      } else {
+        searchPokemon(pokemonID);
+      }
+    });
+  }
+
+  btnSearch();
+
+  function formSearch() {
+    const formSearch = document.querySelector("#pokemonSearch");
+    const pokemonInput = document.querySelector("#pokemonInput");
+    formSearch.addEventListener("submit", (event) => {
+      event.preventDefault();
+      if (pokemonInput.value < 1) {
+      } else {
+        searchPokemon(pokemonInput.value);
+      }
+    });
+  }
+  formSearch();
 }
-
-function insertSprite(pokemon) {
-  const image = pokemon["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
-  const pokemonSprite = document.querySelector(".pokemonSprite");
-  pokemonSprite.setAttribute("src", image);
-}
-
-function insertData(pokemon) {
-  const pokemonName = document.querySelector(".pokemonName");
-  const pokemonType = document.querySelector(".pokemonType");
-  const pokemonLocation = document.querySelector(".pokemonLocation");
-
-  pokemonName.innerText = pokemon["name"];
-  pokemonType.innerText = pokemon["types"]["0"]["type"]["name"];
-  pokemonLocation.innerText = pokemon["id"];
-}
-
-// insertSprite("1");
-
-searchPokemon("11");
+initPokemon();
